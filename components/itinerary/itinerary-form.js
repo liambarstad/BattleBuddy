000a0306088535f7ff90ce1bbba6400807907ae7
Liam Barstad <liambarstad@gmail.com>
2018-02-28 19:27:13 -0700

    delete realm stuff

--- components/itinerary/itinerary-form.js
+++ components/itinerary/itinerary-form.js
@@ -4,14 +4,38 @@ import { ScrollView, View, Text } from 'react-native'
 export default class ItineraryForm extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      editing: false,
+      title: this.props.title,
+      time: this.props.time,
+    }
   }
 
   render() {
     return (
       <View>
         <ScrollView>
-          <Text>FuckYou</Text>
+          <OptionBox
+            name='Title'
+            type='text'
+            popout='true'
+            value={this.state.title}
+            onSubmit={(title) => this.state.title}
+          />
+          <OptionBox
+            name='Task Time'
+            type='task-time'
+            popout='true'
+            value={this.state.time}
+            onSubmit={(time) => this.setState({time})
+          />
         </ScrollView>
+
+        <Button
+          style={itineraryStyles.formSubmit}
+          onPress={this.submit}
+          title='Add Itinerary Item'
+        />
       </View>
     )
   }
